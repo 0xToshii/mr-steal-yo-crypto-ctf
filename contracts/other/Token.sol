61639aa078408545763441184d667e803d747797
0xToshii <94262432+0xToshii@users.noreply.github.com>
2022-11-04 13:23:39 -0700

    safu-vault

---
+++ contracts/other/Token.sol
@@ -0,0 +1,29 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+
+/// @dev implementation for arbitary tokens
+contract Token is ERC20, Ownable {
+
+    constructor(
+        string memory _name,
+        string memory _symbol
+    ) ERC20(
+        _name,
+        _symbol
+    ) {}
+
+    /// @dev mints specified amount for specified users
+    function mintPerUser(
+        address[] calldata users, 
+        uint256[] calldata amounts
+    ) external onlyOwner {
+        for (uint256 i; i<users.length; i++) {
+            _mint(users[i],amounts[i]);
+        }
+    }
+
+}
