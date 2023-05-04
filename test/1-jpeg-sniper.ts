17af71a7999c9dfd0b0bb4c783968c63412a04ff
olajuwon74 <adebayosolomon74@gmail.com>
2023-05-04 01:33:49 +0100

    Nft test

--- test/1-jpeg-sniper.ts
+++ test/1-jpeg-sniper.ts
@@ -1,42 +1,64 @@
-// import { expect } from "chai";
-// import { Contract, Signer, BigNumber } from "ethers";
-// import { ethers } from "hardhat";
+import { expect } from "chai";
+import { Address } from "cluster";
+import { Contract, Signer, BigNumber } from "ethers";
+import { ethers } from "hardhat";
 
-// const BN = BigNumber;
-// let precision = BN.from(10).pow(18);
+const BN = BigNumber;
+let precision = BN.from(10).pow(18);
 
-// let accounts: Signer[];
-// let attacker: Signer;
-// let o1: Signer;
-// let o2: Signer;
-// let admin: Signer; // should not be used
-// let flatLaunchpeg: Contract; 
-// let startBlock: Number;
+let accounts: Signer[];
+let attacker: Signer;
+let o1: Signer;
+let o2: Signer;
+let admin: Signer; // should not be used
+let flatLaunchpeg: Contract; 
+let startBlock: Number;
+let jpegSniperExploiter: Contract;
+let flatLaunchpegAddress: Contract;
 
-// /// preliminary state
-// before(async () => {
+/// preliminary state
+before(async () => {
 
-//   accounts = await ethers.getSigners();
-//   [attacker, o1, o2, admin] = accounts;
+  accounts = await ethers.getSigners();
+  [attacker, o1, o2, admin] = accounts;
 
-//   let flatLaunchpegFactory = await ethers.getContractFactory('FlatLaunchpeg')
-//   flatLaunchpeg = await flatLaunchpegFactory.connect(admin).deploy(69,5,5)
+  let flatLaunchpegFactory = await ethers.getContractFactory('FlatLaunchpeg')
+  flatLaunchpeg = await flatLaunchpegFactory.connect(admin).deploy(69,5,5)
+  await flatLaunchpeg.deployed()
+  let flatLaunchpegAddress = await flatLaunchpeg.address
+  console.log(flatLaunchpegAddress)
 
-//   startBlock = await ethers.provider.getBlockNumber()
+  startBlock = await ethers.provider.getBlockNumber()
+  console.log(await startBlock);
+  console.log(await flatLaunchpeg.connect(attacker).numberMinted(attacker.getAddress()))
   
-// });
+});
 
-// it("solves the challenge", async function () {
+it("solves the challenge", async function () {
 
-//   // implement solution here
+  // implement solution here
 
-// });
+ 
 
-// /// expected final state
-// after(async () => {
+  let jpegSniperExploiterFactory = await ethers.getContractFactory('jpegSniperExploiter')
+  console.log(await attacker.getAddress())
+  jpegSniperExploiter = await jpegSniperExploiterFactory.connect(attacker).deploy(flatLaunchpeg.address, attacker.getAddress())
+  await jpegSniperExploiter.deployed()
+  console.log(await jpegSniperExploiter.address);
 
-//   expect(await flatLaunchpeg.totalSupply()).to.be.equal(69)
-//   expect(await flatLaunchpeg.balanceOf(await attacker.getAddress())).to.be.equal(69)
-//   expect(await ethers.provider.getBlockNumber()).to.be.equal(startBlock+1)
+//   jpegSniperExploiter.connect(attacker)._runExploit(flatLaunchpeg.address, attacker.getAddress())
+//   console.log("success")
+ console.log(await flatLaunchpeg.totalSupply()); 
+ console.log(await flatLaunchpeg.connect(attacker).numberMinted(attacker.getAddress()))
+
+
+});
+
+/// expected final state
+after(async () => {
+
+  expect(await flatLaunchpeg.totalSupply()).to.be.equal(69)
+  expect(await flatLaunchpeg.balanceOf(await attacker.getAddress())).to.be.equal(69)
+  expect(await ethers.provider.getBlockNumber()).to.be.equal(startBlock+1)
   
-// });
+});
--- test/7-freebie.ts
+++ test/7-freebie.ts
@@ -1,132 +1,132 @@
-import { expect } from "chai";
-import { Contract, Signer, BigNumber } from "ethers";
-import { ethers } from "hardhat";
-import { start } from "repl";
-import { contracts, Farm__factory } from "../typechain-types";
-import { erc20 } from "../typechain-types/@openzeppelin/contracts/token";
+// import { expect } from "chai";
+// import { Contract, Signer, BigNumber } from "ethers";
+// import { ethers } from "hardhat";
+// import { start } from "repl";
+// import { contracts, Farm__factory } from "../typechain-types";
+// import { erc20 } from "../typechain-types/@openzeppelin/contracts/token";
 
 
-  const BN = BigNumber;
-  let precision = BN.from(10).pow(18);
+//   const BN = BigNumber;
+//   let precision = BN.from(10).pow(18);
   
-  let accounts: Signer[];
-  let attacker: Signer;
-  let o1: Signer;
-  let o2: Signer;
-  let admin: Signer; // should not be used
-  let adminUser: Signer; // should not be used
-  let govToken: Contract;
-  let rewardsAdvisor: Contract;
-  let farm: Contract; // farm token
-  let scam: Contract;
+//   let accounts: Signer[];
+//   let attacker: Signer;
+//   let o1: Signer;
+//   let o2: Signer;
+//   let admin: Signer; // should not be used
+//   let adminUser: Signer; // should not be used
+//   let govToken: Contract;
+//   let rewardsAdvisor: Contract;
+//   let farm: Contract; // farm token
+//   let scam: Contract;
 
 
 
 
-/// preliminary state
-before(async () => {
+// /// preliminary state
+// before(async () => {
 
-  accounts = await ethers.getSigners();
-  [attacker, o1, o2, admin, adminUser] = accounts;
+//   accounts = await ethers.getSigners();
+//   [attacker, o1, o2, admin, adminUser] = accounts;
 
-  // deploying `FARM` token contract
-  let farmTokenFactory = await ethers.getContractFactory('Farm')
-  console.log("deploying token...");
-  farm = await farmTokenFactory.connect(admin).deploy();
-  await farm.deployed();
-  const farmAddress = (farm.address);
-  console.log(`farmToken deployed to: ${farmAddress}`);
-  const name = await farm.name();
-  console.log(`Token Name: ${name}\n`);
-  const decimals = await farm.decimals();
-  console.log(`Token Decimal: ${decimals}\n`);
-  console.log('Querying token supply...');
-
-
-
-  const farmT = await ethers.getContractAt("Farm", farmAddress);
-  const symbol = await farmT.symbol();
-  console.log(`Token Symbol: ${symbol}\n`);
-
-  const totalSupply = await farmT.totalSupply();
-  console.log(await `Token TOTALSUPPLY: ${totalSupply}\n`);
-
-  console.log('Getting the balance of contract owner...');
-  const ownerAddress = await admin.getAddress();
-  console.log(ownerAddress)
-  const ownerBalance = await farm.balanceOf(ownerAddress);
-  console.log( await `Contract owner at ${ownerAddress} has a ${symbol} balance of ${ethers.utils.formatUnits(ownerBalance, decimals)}\n`);
-
-  await farmT.connect(admin).transfer(await adminUser.getAddress(), precision.mul(10_000) )
-  await farmT.connect(admin).transfer(await attacker.getAddress(), precision.mul(1) )
-
-  console.log(await farm.balanceOf(adminUser.getAddress()));
-  console.log(await farm.balanceOf(attacker.getAddress()));
-  console.log(await farm.balanceOf(admin.getAddress()));
+//   // deploying `FARM` token contract
+//   let farmTokenFactory = await ethers.getContractFactory('Farm')
+//   console.log("deploying token...");
+//   farm = await farmTokenFactory.connect(admin).deploy();
+//   await farm.deployed();
+//   const farmAddress = (farm.address);
+//   console.log(`farmToken deployed to: ${farmAddress}`);
+//   const name = await farm.name();
+//   console.log(`Token Name: ${name}\n`);
+//   const decimals = await farm.decimals();
+//   console.log(`Token Decimal: ${decimals}\n`);
+//   console.log('Querying token supply...');
 
 
-  // deploying protocol contracts
-  let govTokenFactory = await ethers.getContractFactory('GovToken')
-  govToken = await govTokenFactory.connect(admin).deploy("xFARM","xFARM")
-  console.log("govToken deployed to:", govToken.address);
-  console.log(await govToken.owner())
 
-  let rewardsAdvisorFactory = await ethers.getContractFactory('RewardsAdvisor')
-  rewardsAdvisor = await rewardsAdvisorFactory.connect(admin).deploy(farm.address,govToken.address)
-  console.log(await rewardsAdvisor.address)
+//   const farmT = await ethers.getContractAt("Farm", farmAddress);
+//   const symbol = await farmT.symbol();
+//   console.log(`Token Symbol: ${symbol}\n`);
+
+//   const totalSupply = await farmT.totalSupply();
+//   console.log(await `Token TOTALSUPPLY: ${totalSupply}\n`);
+
+//   console.log('Getting the balance of contract owner...');
+//   const ownerAddress = await admin.getAddress();
+//   console.log(ownerAddress)
+//   const ownerBalance = await farm.balanceOf(ownerAddress);
+//   console.log( await `Contract owner at ${ownerAddress} has a ${symbol} balance of ${ethers.utils.formatUnits(ownerBalance, decimals)}\n`);
+
+//   await farmT.connect(admin).transfer(await adminUser.getAddress(), precision.mul(10_000) )
+//   await farmT.connect(admin).transfer(await attacker.getAddress(), precision.mul(1) )
+
+//   console.log(await farm.balanceOf(adminUser.getAddress()));
+//   console.log(await farm.balanceOf(attacker.getAddress()));
+//   console.log(await farm.balanceOf(admin.getAddress()));
 
 
-  await govToken.connect(admin).transferOwnership(rewardsAdvisor.address)
+//   // deploying protocol contracts
+//   let govTokenFactory = await ethers.getContractFactory('GovToken')
+//   govToken = await govTokenFactory.connect(admin).deploy("xFARM","xFARM")
+//   console.log("govToken deployed to:", govToken.address);
+//   console.log(await govToken.owner())
 
-  console.log(await govToken.owner())
+//   let rewardsAdvisorFactory = await ethers.getContractFactory('RewardsAdvisor')
+//   rewardsAdvisor = await rewardsAdvisorFactory.connect(admin).deploy(farm.address,govToken.address)
+//   console.log(await rewardsAdvisor.address)
 
 
-  // other user stakes their `FARM` token
-  let adminUserAddress = await adminUser.getAddress()
-  await farm.connect(adminUser).approve(rewardsAdvisor.address,ethers.constants.MaxUint256)
-  await rewardsAdvisor.connect(adminUser).deposit(precision.mul(10_000),adminUserAddress,adminUserAddress)
-  console.log(await farm.balanceOf(adminUser.getAddress()));
-  console.log(await farm.balanceOf(rewardsAdvisor.address));
-});
+//   await govToken.connect(admin).transferOwnership(rewardsAdvisor.address)
 
-it("solves the challenge", async function () {
+//   console.log(await govToken.owner())
 
-  // implement solution here
 
-  let scamFactory = await ethers.getContractFactory('Scam')
-  scam = await scamFactory.connect(attacker).deploy(rewardsAdvisor.address)
-  await scam.deployed()
-  console.log(await scam.address);
-  
-  console.log(`\n`)
-  console.log(`______Before Attack_______\n`)
+//   // other user stakes their `FARM` token
+//   let adminUserAddress = await adminUser.getAddress()
+//   await farm.connect(adminUser).approve(rewardsAdvisor.address,ethers.constants.MaxUint256)
+//   await rewardsAdvisor.connect(adminUser).deposit(precision.mul(10_000),adminUserAddress,adminUserAddress)
+//   console.log(await farm.balanceOf(adminUser.getAddress()));
+//   console.log(await farm.balanceOf(rewardsAdvisor.address));
+// });
+
+// it("solves the challenge", async function () {
+
+//   // implement solution here
+
+//   let scamFactory = await ethers.getContractFactory('Scam')
+//   scam = await scamFactory.connect(attacker).deploy(rewardsAdvisor.address)
+//   await scam.deployed()
+//   console.log(await scam.address);
+
+//   console.log(`\n`)
+//   console.log(`______Before Attack_______\n`)
  
-  console.log(await farm.balanceOf(rewardsAdvisor.address));
-  console.log(await farm.balanceOf(attacker.getAddress()));
-
- 
-  console.log(`\n`)
-
-  await scam.connect(attacker).steal()
-  await scam.connect(attacker).steal()
-  await scam.connect(attacker).steal()
+//   console.log(await farm.balanceOf(rewardsAdvisor.address));
+//   console.log(await farm.balanceOf(attacker.getAddress()));
 
  
+//   console.log(`\n`)
+
+//   await scam.connect(attacker).steal()
+//   await scam.connect(attacker).steal()
+//   await scam.connect(attacker).steal()
+
+ 
 
 
-  console.log(await farm.balanceOf(rewardsAdvisor.address));
-  console.log(await farm.balanceOf(attacker.getAddress()));
+//   console.log(await farm.balanceOf(rewardsAdvisor.address));
+//   console.log(await farm.balanceOf(attacker.getAddress()));
 
-  console.log("After attack was called!!!!!!!!!!!")
+//   console.log("After attack was called!!!!!!!!!!!")
 
-});
+// });
 
 
-/// expected final state
-  after(async () => {
+// /// expected final state
+//   after(async () => {
 
-    // attacker drains 99.99%+ of the `FARM` tokens from RewardsAdvisor staking contract
-    expect(await farm.balanceOf(await attacker.getAddress())).to.be.gte(precision.mul(10_000))
-    expect(await farm.balanceOf(rewardsAdvisor.address)).to.be.lte(precision.mul(1))
+//     // attacker drains 99.99%+ of the `FARM` tokens from RewardsAdvisor staking contract
+//     expect(await farm.balanceOf(await attacker.getAddress())).to.be.gte(precision.mul(10_000))
+//     expect(await farm.balanceOf(rewardsAdvisor.address)).to.be.lte(precision.mul(1))
 
-  });
+//   });
